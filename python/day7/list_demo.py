f38670643722b8facab2fca68019b1a8db5d9a1b
kkun56747-lang <kkun56747@gmail.com>
2026-07-02 13:18:23 +0800

    Day7: Learn for list_demo

---
+++ python/day7/list_demo.py
@@ -0,0 +1,9 @@
+tasks = []
+count = int(input("请输入今天几个任务："))
+
+for i in range(1, count + 1):
+    task = input(f"请输入第 {i} 个任务：")
+    tasks.append(task)
+
+for i in range(1, len(tasks) + 1):
+    print(f"{i}.{tasks[i-1]}")
