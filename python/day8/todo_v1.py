483dcd86814c05bce3f32e8c20b418f309824112
kkun56747-lang <kkun56747@gmail.com>
2026-07-03 20:09:24 +0800

    Day 9: Todo List V2 - Delete task

--- python/day8/todo_v1.py
+++ python/day8/todo_v1.py
@@ -3,7 +3,8 @@ while True:
     print("===========Todo List===========")
     print("1. 添加任务")
     print("2. 查看任务")
-    print("3. 退出")
+    print("3. 删除任务")
+    print("4. 退出")
 
     choice = input("请输入选择：")
 
@@ -12,10 +13,37 @@ while True:
         tasks.append(input_task)
         print("添加成功！")
 
-    if choice == "2":
-        print("当前任务：")
-        for i in range(1, len(tasks) + 1):
+        
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("当前没有任务")
+        else:
+            print("当前任务：")
+        for i in range(1,len(tasks)+1):
             print(f"{i}.{tasks[i-1]}")
-    if choice == "3":
+        
+
+    elif choice == "3":
+
+        if len(tasks) == 0:
+            print("当前没有任务")
+        else: 
+            
+            print("当前任务：")
+            for i in range(1,len(tasks)+1):
+              print(f"{i}.{tasks[i-1]}")
+              print("请输入删除编号：")
+
+            if  len(tasks) == 0:
+              print("当前没有任务可以删除！")
+
+            else:
+              delete = input()
+              tasks.pop(int(delete) - 1)
+              print("删除成功！")
+
+    elif choice == "4":
         print("程序结束")
         break
+
+    print("============返回上级菜单============")
