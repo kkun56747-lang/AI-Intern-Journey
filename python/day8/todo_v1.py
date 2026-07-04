2cb717f88fa6ebce6520f1608556475d0e51db8c
kkun56747-lang <kkun56747@gmail.com>
2026-07-04 17:39:09 +0800

    Day 9: Todo List V2 - Delete task

--- python/day8/todo_v1.py
+++ python/day8/todo_v1.py
@@ -19,28 +19,20 @@ while True:
             print("当前没有任务")
         else:
             print("当前任务：")
-        for i in range(1,len(tasks)+1):
-            print(f"{i}.{tasks[i-1]}")
+            for i in range(1,len(tasks)+1):
+             print(f"{i}.{tasks[i-1]}")
         
 
     elif choice == "3":
-
         if len(tasks) == 0:
             print("当前没有任务")
         else: 
-            
             print("当前任务：")
             for i in range(1,len(tasks)+1):
               print(f"{i}.{tasks[i-1]}")
-              print("请输入删除编号：")
-
-            if  len(tasks) == 0:
-              print("当前没有任务可以删除！")
-
-            else:
-              delete = input()
-              tasks.pop(int(delete) - 1)
-              print("删除成功！")
+            delete = input("请输入删除编号：")
+            tasks.pop(int(delete) - 1)
+            print("删除成功！")
 
     elif choice == "4":
         print("程序结束")
